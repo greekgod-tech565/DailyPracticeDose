072dccf05d23d9ce3e0b917c0e89d40f68e81832
greekgod-tech565 <memesgot08@gmail.com>
2026-05-05 23:52:14 +0530

    Implement search in rotated sorted array
    Key Concepts:
    - Binary Search
    - Rotated Sorted Array logic
    - Identifying sorted halves
    
    Approach:
    - Check which half is sorted (left or right)
    - Decide where the target lies
    - Reduce search space accordingly

---
+++ Search_RotatedSortedArray.java
@@ -0,0 +1,37 @@
+class RotatedSearch {
+
+    static int search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) return mid;
+
+            // left sorted
+            if (arr[low] <= arr[mid]) {
+                if (target >= arr[low] && target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // right sorted
+            else {
+                if (target > arr[mid] && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+
+        System.out.println(search(arr, target));
+    }
+}
