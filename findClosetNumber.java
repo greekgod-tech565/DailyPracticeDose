60491956059e3998a47de8fd3787aa5071ad1064
greekgod-tech565 <memesgot08@gmail.com>
2026-05-15 22:57:59 +0530

    Update findClosetNumber.java
    Approach
    1. Sort the array
    2. Fix one element using a loop
    3. Use left and right pointers to find the closest sum
    4. Compare current sum with previous closest answer
    5. Update closestSum when a better answer is found
    
    
    This problem helped me understand:
    - Two Pointer Technique
    - Closest value comparison
    - Math.abs() usage
    - Optimization in array problems

--- findClosetNumber.java
+++ findClosetNumber.java
@@ -1,5 +1,5 @@
  int[] arr = {-1,2,1,-4}; int target =1;
-        Arrays.sort(arr); int closetSum = arr[0]+arr[1]+arr[1];
+        Arrays.sort(arr); int closetSum = arr[0]+arr[1]+arr[2];
         for (int i = 0; i < arr.length ; i++) {
 
             if((i > 0 && arr[i] == arr[i-1])){
@@ -17,7 +17,7 @@
                     left++ ; right--;
                 } else if (sum < 0) {
 
-                    left++;
+                     left++;
                 } else {
                     right--;
                 }
