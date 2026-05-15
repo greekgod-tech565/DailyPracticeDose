9a957d7caf90ad8351a269aa57c5990536214ed5
greekgod-tech565 <memesgot08@gmail.com>
2026-05-15 22:25:30 +0530

    Add function to find closest sum to target
    Implement a function to find the closest sum to a target from an array of integers.

---
+++ findClosetNumber.java
@@ -0,0 +1,28 @@
+ int[] arr = {-1,2,1,-4}; int target =1;
+        Arrays.sort(arr); int closetSum = arr[0]+arr[1]+arr[1];
+        for (int i = 0; i < arr.length ; i++) {
+
+            if((i > 0 && arr[i] == arr[i-1])){
+                continue;
+            }
+             int left = i+1;
+             int right= arr.length-1;
+             while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                 if(Math.abs(sum-target)<Math.abs(closetSum-target)){
+                     closetSum=sum;
+                 }
+                if (sum == 1) {
+                   // System.out.println( " [ " + arr[i] + " " + arr[left] + " " + arr[right] + " ] ");
+                    left++ ; right--;
+                } else if (sum < 0) {
+
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        System.out.println(closetSum);
+    }
+}
