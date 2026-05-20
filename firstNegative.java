02229593a6f166c75d2ac1d62e7e7d8b0377f8af
greekgod-tech565 <memesgot08@gmail.com>
2026-05-20 21:38:55 +0530

    Fix logic for finding first negative in sliding window

--- firstNegative.java
+++ firstNegative.java
@@ -1,19 +1,36 @@
+     public class Main {
+
+    public static void main(String[] args) {
+
         int[] arr = {12, -1, -7, 8, -15, 30, 16, 28};
+
         int k = 3;
+
         int low = 0;
-        for (int high = k-1; high < arr.length; high++) {
+
+        for (int high = k - 1; high < arr.length; high++) {
+
             boolean found = false;
-            while (low < high) {
-                if (arr[low] < 0) {
-                    System.out.print(arr[low] + " ");
+
+            // Current window check
+            for (int i = low; i <= high; i++) {
+
+                if (arr[i] < 0) {
+
+                    System.out.print(arr[i] + " ");
+
                     found = true;
+
                     break;
-                } else {
-                    low++;
                 }
             }
-            if(found == false){
-                System.out.print(0+" ");
+
+            if (found == false) {
+                System.out.print(0 + " ");
             }
+
+            // Slide window
             low++;
         }
+    }
+}
