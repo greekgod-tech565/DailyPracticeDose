d263869266b030d33584f74cfdc7f69e5f5ee57d
greekgod-tech565 <memesgot08@gmail.com>
2026-09-01 11:02:31 -0700

    Implement sum of prime numbers up to N

---
+++ SumOfPrimeNumberToN.java
@@ -0,0 +1,20 @@
+int n = 10;
+int sum = 0;
+
+for (int i = 2; i <= n; i++) {
+
+    boolean isPrime = true;
+
+    for (int j = 2; j * j <= i; j++) {
+        if (i % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        sum += i;
+    }
+}
+
+System.out.println(sum);
