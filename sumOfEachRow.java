8087d630be9ddbe2b6c75747cbcdd3be30e6f654
greekgod-tech565 <memesgot08@gmail.com>
2026-09-04 11:14:53 -0700

    Implement sum of each row in a 2D array

---
+++ sumOfEachRow.java
@@ -0,0 +1,16 @@
+int[][] arr = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+for (int i = 0; i < arr.length; i++) {
+
+    int sum = 0;
+
+    for (int j = 0; j < arr[i].length; j++) {
+        sum = sum + arr[i][j];
+    }
+
+    System.out.println("Row " + i + " sum = " + sum);
+}
