d674db181fc62e6bf87c8f35b52809cc27214c64
greekgod-tech565 <memesgot08@gmail.com>
2026-09-12 08:03:28 -0700

    Implement search for element in 2D array

---
+++ Search an element in 2D array.java
@@ -0,0 +1,17 @@
+int target = 5;
+boolean found = false;
+
+for (int i = 0; i < arr.length; i++) {
+    for (int j = 0; j < arr[i].length; j++) {
+
+        if (arr[i][j] == target) {
+            System.out.println("Found at row " + i + ", column " + j);
+            found = true;
+        }
+
+    }
+}
+
+if (!found) {
+    System.out.println("Not found");
+}
